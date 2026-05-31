bb5fbff994fb104955fa4c59a0e33dcb7915ba8e
Ariel Viñas <vinas.dardoariel@gmail.com>
2026-05-30 22:45:46 -0300

    Initial Commit

---
+++ Maraton/MaratonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaratonApp.swift
+//  Maraton
+//
+//  Created by Ariel Viñas on 30/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MaratonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
